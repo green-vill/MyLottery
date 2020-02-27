4ccb0e40dda36fb322c6fc944274dbb1cb9edea1
Darren Chen <darren.x.chen.-nd@disney.com>
2020-02-27 17:37:11 +0800

    算法完善

--- src/main/java/com/lucky/test/LuckDrawMain.java
+++ src/main/java/com/lucky/test/LuckDrawMain.java
@@ -7,34 +7,37 @@ import java.util.stream.Collectors;
 public class LuckDrawMain {
 
     public static void main(String[] args) {
-        List<Person> personList = new ArrayList<>();
-        Person person1 = new Person();
-        person1.setId(1);
-        person1.setName("person1");
-        person1.setVid("v001");
+        List<Person> personList1 = getPersonGroup(0, 0);
+        List<Person> personList2 = getPersonGroup(1, 2);
+        List<Person> personList3 = getPersonGroup(3, 5);
+        List<Person> personList4 = getPersonGroup(6, 9);
+        List<Person> personList5 = getPersonGroup(10, 14);
 
-        Person person2 = new Person();
-        person2.setId(2);
-        person2.setName("person2");
-        person2.setVid("v002");
+        LotteryConfig lotteryConfig = new LotteryConfig();
+        RandomService randomService = new RandomService(lotteryConfig.getGuestForeCast());
 
-        Person person3 = new Person();
-        person3.setId(3);
-        person3.setName("person3");
-        person3.setVid("v003");
+        generateNumbers(personList1, randomService, lotteryConfig);
+        generateNumbers(personList2, randomService, lotteryConfig);
+        generateNumbers(personList3, randomService, lotteryConfig);
+        generateNumbers(personList4, randomService, lotteryConfig);
+        generateNumbers(personList5, randomService, lotteryConfig);
 
-        personList.add(person1);
-        personList.add(person2);
-        personList.add(person3);
-
-        generateNumbers(personList);
     }
 
-    public static void generateNumbers(List<Person> personList) {
+    private static List<Person> getPersonGroup(int start, int end) {
+        List<Person> personList = new ArrayList<>();
+        for (int i = start; i <= end; i++) {
+            Person person = new Person();
+            person.setId(i);
+            person.setName("name" + i);
+            person.setVid("vid-" + i);
+            personList.add(person);
+        }
+        return personList;
+    }
+
+    public static void generateNumbers(List<Person> personList, RandomService randomService, LotteryConfig lotteryConfig) {
         long startTime = System.currentTimeMillis();
-        LotteryConfig lotteryConfig = new LotteryConfig();
-        int guestForeCast = lotteryConfig.getGuestForeCast();
-        RandomService randomService = new RandomService(guestForeCast);
         if (personList == null || personList.size() == 0) {
             return;
         }
@@ -45,9 +48,14 @@ public class LuckDrawMain {
         for (LotteryConfig.Prize prize : prizes) {
             if (firstNumber >= prize.getStart() && firstNumber < prize.getEnd()) {
                 if (prize.isGroupActivity()) {
-                    if (personSize <= prize.getInventory()) {
+                    if (personSize - 1 <= prize.getInventory()) {
                         randomService.guestNos.remove(firstNumber);
-                        List<Integer> leftNumbers = randomService.getGuestNumbers(personSize - 1, null);
+                        List<Integer> groupActivityNumbers = new ArrayList<>();
+                        for (int i = prize.getStart(); i < prize.getEnd(); i++) {
+                            groupActivityNumbers.add(i);
+                        }
+                        groupActivityNumbers.remove(firstNumber);
+                        List<Integer> leftNumbers = randomService.getGuestNumbers(personSize - 1, groupActivityNumbers);
                         for (Integer number : leftNumbers) {
                             randomService.guestNos.remove(number);
                         }
@@ -63,17 +71,23 @@ public class LuckDrawMain {
                         getNoneGroupActivityNumbers(randomService, prizes, personList);
                     }
                 } else {
-                    List<Integer> availableGuestNos = getAvailableNumbers(randomService.guestNos, prizes);
-                    List<Integer> numbers = randomService.getGuestNumbers(personList.size() - 1, availableGuestNos);
-                    for (int i = 0; i < personSize; i++) {
-                        if (i == 0) {
-                            Person person = personList.get(i);
-                            setPersonPrizeAndNumber(person, firstNumber, prizes, randomService);
-                        } else {
-                            Integer number = numbers.get(i - 1);
-                            Person person = personList.get(i);
-                            setPersonPrizeAndNumber(person, number, prizes, randomService);
+                    if (personSize - 1 <= prize.getInventory()) {
+                        randomService.guestNos.remove(firstNumber);
+                        List<Integer> availableGuestNos = getAvailableNumbers(randomService.guestNos, prizes, personSize - 1);
+                        availableGuestNos.remove(firstNumber);
+                        List<Integer> numbers = randomService.getGuestNumbers(personList.size() - 1, availableGuestNos);
+                        for (int i = 0; i < personSize; i++) {
+                            if (i == 0) {
+                                Person person = personList.get(i);
+                                setPersonPrizeAndNumber(person, firstNumber, prizes, randomService, false);
+                            } else {
+                                Integer number = numbers.get(i - 1);
+                                Person person = personList.get(i);
+                                setPersonPrizeAndNumber(person, number, prizes, randomService, true);
+                            }
                         }
+                    } else {
+                        getNoneGroupActivityNumbers(randomService, prizes, personList);
                     }
                 }
                 break;
@@ -83,16 +97,16 @@ public class LuckDrawMain {
     }
 
     private static void getNoneGroupActivityNumbers(RandomService randomService, List<LotteryConfig.Prize> prizes, List<Person> personList) {
-        List<Integer> availableGuestNos = getAvailableNumbers(randomService.guestNos, prizes);
+        List<Integer> availableGuestNos = getAvailableNumbers(randomService.guestNos, prizes, personList.size());
         List<Integer> numbers = randomService.getGuestNumbers(personList.size(), availableGuestNos);
         for (int i = 0; i < numbers.size(); i++) {
             Integer number = numbers.get(i);
             Person person = personList.get(i);
-            setPersonPrizeAndNumber(person, number, prizes, randomService);
+            setPersonPrizeAndNumber(person, number, prizes, randomService, true);
         }
     }
 
-    private static List<Integer> getAvailableNumbers(List<Integer> guestNos, List<LotteryConfig.Prize> prizes) {
+    private static List<Integer> getAvailableNumbers(List<Integer> guestNos, List<LotteryConfig.Prize> prizes, int size) {
         List<Integer> availableGuestNos = new ArrayList<>();
         List<LotteryConfig.Prize> groupActivityPrizes = prizes.stream().filter(p -> p.isGroupActivity()).collect(Collectors.toList());
         for (Integer no : guestNos) {
@@ -100,10 +114,20 @@ public class LuckDrawMain {
                 availableGuestNos.add(no);
             }
         }
+        List<LotteryConfig.Prize> noneGroupActivityNotEnoughPrizes = prizes
+                .stream()
+                .filter(p -> !p.isGroupActivity() && p.getInventory() < size)
+                .collect(Collectors.toList());
+        for (int i = availableGuestNos.size() - 1; i >= 0; i--) {
+            Integer number = availableGuestNos.get(i);
+            if (noneGroupActivityNotEnoughPrizes.stream().anyMatch(p -> p.getStart() <= number && p.getEnd() > number)) {
+                availableGuestNos.remove(number);
+            }
+        }
         return availableGuestNos;
     }
 
-    private static void setPersonPrizeAndNumber(Person person, Integer number, List<LotteryConfig.Prize> prizes, RandomService randomService) {
+    private static void setPersonPrizeAndNumber(Person person, Integer number, List<LotteryConfig.Prize> prizes, RandomService randomService, boolean doRemove) {
         person.setNumber(number);
         for (LotteryConfig.Prize prize1 : prizes) {
             if (prize1.getStart() <= number && prize1.getEnd() > number) {
@@ -111,6 +135,8 @@ public class LuckDrawMain {
                 prize1.setInventory(prize1.getInventory() - 1);
             }
         }
-        randomService.guestNos.remove(number);
+        if (doRemove) {
+            randomService.guestNos.remove(number);
+        }
     }
 }
